36941632b0b414f3b5a4214544304f353c272cf6
Andrew Moore <mooreandrewlr@gmail.com>
2015-01-14 17:17:14 +0000

    Fixed Style Path

--- propertyfrontend/config.rb
+++ propertyfrontend/config.rb
@@ -6,6 +6,6 @@ set :bind, '0.0.0.0'
 puts ENV['port']
 set :port, (ARGV[0] || '4567')
 
-set :public_folder, 'public'
+set :public_folder, File.dirname(__FILE__) + '/public'
 
 require_relative 'server.rb'
